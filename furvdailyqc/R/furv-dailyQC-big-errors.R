3a045b0fa58d215c1f76388839a5d64e8e44663f
Alba Gilabert Gallart <alba.gilabert@urv.cat>
2018-07-03 11:54:50 +0200

    User can choose the upper and lower threshold for each variable. use the dataset previously uploaded if exists. Yuri's comment

--- furvdailyqc/R/furv-dailyQC-big-errors.R
+++ furvdailyqc/R/furv-dailyQC-big-errors.R
@@ -4,16 +4,15 @@
 #' given threshold.
 #'
 #' Given daily maximum (Tmax) and minimum (Tmin) temperatures and daily precipitation
-#' (RR) values, reports RR values exceeding 200mm and Tmax and Tmin values exceeding
-#' 50 degC.The output is a list with the days in which RR or Tmax or Tmin exceeds
+#' (RR) values, reports RR, and Tmax and Tmin values exceeding thresholds selected
+#' by the suer.The output is a list with the days in which RR or Tmax or Tmin exceeds
 #' these thresholds.
 #'
 #' @details Input: A text file: each line is for one calendar day.
 #'   The order of the columns should be: Year - Month - RR - Tmax - Tmin.
 #'   Without header and NA values = -99.9.
 #'
-#'   Output: writes a list with suspicious values (RR > 200 mm or Tmax > 50 degC
-#'   or Tmax < -50 degC or Tmin > 50 degC or Tmin < -50 degC) in <out_of_range.txt>;
+#'   Output: writes a list with suspicious values in <out_of_range.txt>;
 #'   each line represents one day and the order of columns is: Year - Month -
 #'   RR - Tmax - Tmin.
 #'
