dc8846dbe925b6f844c59a198db245ff1ac854e6
Alba Gilabert Gallart <alba.gilabert@urv.cat>
2018-07-03 12:24:17 +0200

    Use the dataset previously uploaded if exist. Yuri's comment

--- furvdailyqc/R/furv-dailyQC-climatic-outliers.R
+++ furvdailyqc/R/furv-dailyQC-climatic-outliers.R
@@ -33,18 +33,24 @@
 climatic_outliers <- function() {
     name <- paste("climatic_outliers_boxplots.pdf", sep = "")
     pdf(file = name)
-    data <- read.table(file.choose(),
-      col.names = c("year", "month", "day", "rr", "tmax", "tmin"),
-      na.strings = "-99.9")
-    no_natmax <- length(subset(data$tmax, !is.na(data$tmax)))
-    no_natmin <- length(subset(data$tmin, !is.na(data$tmin)))
-    no_narr <- length(subset(data$rr, !is.na(data$rr)))
+    if (exists('daily_data')) {
+      daily_data <- daily_data
+    } else {
+      daily_data_file <- file.choose()
+      daily_data <- read.table(daily_data_file, 
+        col.names = c("year", "month", "day", "rr", "tmax", "tmin"),
+        na.strings = "-99.9")
+      assign("daily_data", daily_data, envir=globalenv())
+    }
+    no_natmax <- length(subset(daily_data$tmax, !is.na(daily_data$tmax)))
+    no_natmin <- length(subset(daily_data$tmin, !is.na(daily_data$tmin)))
+    no_narr <- length(subset(daily_data$rr, !is.na(daily_data$rr)))
     outrange <- 3
     if (no_natmax > 0 & no_natmin > 0) {
-        data$dtr <- data$tmax - data$tmin
+      daily_data$dtr <- daily_data$tmax - daily_data$tmin
     }
     if (no_narr > 0) {
-        rr_noz <- subset(data, data$rr > 0)
+        rr_noz <- subset(daily_data, daily_data$rr > 0)
     }
     par(mfrow = c(2, 2))
     if (no_narr > 0) {
@@ -52,15 +58,15 @@ climatic_outliers <- function() {
           col = "blue", range = outrange + 2)
     }
     if (no_natmax > 0) {
-        boxplot_tmax <- boxplot(data$tmax ~ data$month, main = "Tmax",
+        boxplot_tmax <- boxplot(daily_data$tmax ~ daily_data$month, main = "Tmax",
           col = "red", range = outrange)
     }
     if (no_natmin > 0) {
-        boxplot_tmin <- boxplot(data$tmin ~ data$month, main = "Tmin",
+        boxplot_tmin <- boxplot(daily_data$tmin ~ daily_data$month, main = "Tmin",
           col = "cyan", range = outrange)
     }
     if (no_natmax > 0 & no_natmin > 0) {
-        boxplot_dtr <- boxplot(data$dtr ~ data$month, main = "DTR",
+        boxplot_dtr <- boxplot(daily_data$dtr ~ daily_data$month, main = "DTR",
           col = "yellow", range = outrange)
     }
 
@@ -73,15 +79,15 @@ climatic_outliers <- function() {
           row.names = FALSE, col.names = FALSE)
         mis_months <- NULL
         for (months in 1:12) {
-            out_rr <- subset(data$rr, data$month == months & data$rr > 0)
+            out_rr <- subset(daily_data$rr, daily_data$month == months & daily_data$rr > 0)
             if (length(out_rr) > 0) {
                 mis_months <- c(mis_months, months)
             }
         }
         baximo <- length(mis_months)
         for (a in 1:baximo) {
-            out_rr <- subset(data, data$month == mis_months[a] &
-                data$rr > boxplot_rr$stats[5, a])
+            out_rr <- subset(daily_data, daily_data$month == mis_months[a] &
+                daily_data$rr > boxplot_rr$stats[5, a])
             write.table(out_rr, file = file_name, append = TRUE, quote = FALSE,
               row.names = FALSE, col.names = FALSE)
         }
@@ -91,16 +97,16 @@ climatic_outliers <- function() {
         write.table("Tmax up", file = file_name, append = TRUE, quote = FALSE,
           row.names = FALSE, col.names = FALSE)
         for (a in 1:12) {
-            out_tmax <- subset(data, data$month == a &
-                data$tmax > boxplot_tmax$stats[5, a])
+            out_tmax <- subset(daily_data, daily_data$month == a &
+                daily_data$tmax > boxplot_tmax$stats[5, a])
             write.table(out_tmax, file = file_name, append = TRUE,
               quote = FALSE, row.names = FALSE, col.names = FALSE)
         }
         write.table("Tmax low", file = file_name, append = TRUE, quote = FALSE,
           row.names = FALSE, col.names = FALSE)
         for (a in 1:12) {
-            out_tmax <- subset(data, data$month == a &
-                data$tmax < boxplot_tmax$stats[1, a])
+            out_tmax <- subset(daily_data, daily_data$month == a &
+                daily_data$tmax < boxplot_tmax$stats[1, a])
             write.table(out_tmax, file = file_name, append = TRUE,
               quote = FALSE, row.names = FALSE, col.names = FALSE)
         }
@@ -110,16 +116,16 @@ climatic_outliers <- function() {
         write.table("Tmin up", file = file_name, append = TRUE, quote = FALSE,
           row.names = FALSE, col.names = FALSE)
         for (a in 1:12) {
-            out_tmin <- subset(data, data$month == a &
-                data$tmin > boxplot_tmin$stats[5, a])
+            out_tmin <- subset(daily_data, daily_data$month == a &
+                daily_data$tmin > boxplot_tmin$stats[5, a])
             write.table(out_tmin, file = file_name, append = TRUE,
               quote = FALSE, row.names = FALSE, col.names = FALSE)
         }
         write.table("Tmin low", file = file_name, append = TRUE, quote = FALSE,
           row.names = FALSE, col.names = FALSE)
         for (a in 1:12) {
-            out_tmin <- subset(data, data$month == a &
-                data$tmin < boxplot_tmin$stats[1, a])
+            out_tmin <- subset(daily_data, daily_data$month == a &
+                daily_data$tmin < boxplot_tmin$stats[1, a])
             write.table(out_tmin, file = file_name, append = TRUE,
               quote = FALSE, row.names = FALSE, col.names = FALSE)
         }
@@ -129,16 +135,16 @@ climatic_outliers <- function() {
         write.table("DTR up", file = file_name, append = TRUE, quote = FALSE,
           row.names = FALSE, col.names = FALSE)
         for (a in 1:12) {
-            out_dtr <- subset(data, data$month == a &
-                data$dtr > boxplot_dtr$stats[5, a])
+            out_dtr <- subset(daily_data, daily_data$month == a &
+                daily_data$dtr > boxplot_dtr$stats[5, a])
             write.table(out_dtr, file = file_name, append = TRUE, quote = FALSE,
               row.names = FALSE, col.names = FALSE)
         }
         write.table("DTR low", file = file_name, append = TRUE, quote = FALSE,
           row.names = FALSE, col.names = FALSE)
         for (a in 1:12) {
-            out_dtr <- subset(data, data$month == a &
-                data$dtr < boxplot_dtr$stats[1, a])
+            out_dtr <- subset(daily_data, daily_data$month == a &
+                daily_data$dtr < boxplot_dtr$stats[1, a])
             write.table(out_dtr, file = file_name, append = TRUE, quote = FALSE,
               row.names = FALSE, col.names = FALSE)
         }
